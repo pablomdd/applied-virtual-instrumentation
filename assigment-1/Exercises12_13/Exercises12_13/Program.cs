2cbe3dbe13b5aa50f0a6995922d0f06d010e8c29
pablomdd <pablo.mdd.007@gmail.com>
2021-09-08 21:08:50 -0500

    add pathLength c#

---
+++ assigment-1/Exercises12_13/Exercises12_13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercises12_13
+{
+    class Program
+    {
+        static double PathLength(double[] x, double[] y)
+        {
+            double length = 0.0;
+            for(int i = 1; i < x.Length; ++i)
+            {
+                length += Math.Sqrt(Math.Pow(x[i] - x[i - 1], 2) + Math.Pow(y[i] - y[i - 1], 2));
+            }
+            return length;
+        }
+
+        static void TestPathLength()
+        {
+            double L2 = 5;
+            bool expectedResult = true;
+
+            double[] x = new double[] { 0, 3 };
+            double[] y = new double[] { 0, 4 };
+            Console.WriteLine("Expect test to be " + expectedResult);
+            Console.WriteLine("Test Result is: " + (PathLength(x, y) == L2));
+        }
+        static void Main(string[] args)
+        {
+            TestPathLength();
+        }
+    }
+}
