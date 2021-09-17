7b31474e4da6dbcc4754f874d4ca2cb10e48014b
pablomdd <pablo.mdd.007@gmail.com>
2021-09-17 17:28:10 -0500

    add polynomial c#

---
+++ class-notes/Polynomial/Polynomial/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Polynomial
+{
+    class  Polynomial
+    {
+        public readonly double[] coeff;
+        string s;
+
+        public Polynomial(double[] coeff)
+        {
+            this.coeff = coeff;
+        }
+
+        public double Eval(double x)
+        {
+            double result = 0;
+            for(int i = 0; i < coeff.Length; ++i)
+            {
+                result += coeff[i] * Math.Pow(x, i);
+            }
+            return result;
+        }
+
+        public static Polynomial operator +(Polynomial a, Polynomial b)
+        {
+            double[] result;
+            result = new double[Math.Max(a.coeff.Length, b.coeff.Length)];
+            if (a.coeff.Length > b.coeff.Length)
+            {
+                for(int i = 0; i < a.coeff.Length; ++i)
+                {
+                    result[i] = a.coeff[i];
+                }
+                for (int i = 0; i < b.coeff.Length; ++i)
+                {
+                    result[i] += b.coeff[i];
+                }
+            } else
+            {
+                for (int i = 0; i < b.coeff.Length; ++i)
+                {
+                    result[i] = b.coeff[i];
+                }
+                for (int i = 0; i < a.coeff.Length; ++i)
+                {
+                    result[i] += a.coeff[i];
+                }
+            }
+            Console.WriteLine(result);
+            return new Polynomial(result);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] cp1 = new double[] { 1, -1 };
+            Polynomial p1 = new Polynomial(cp1);
+            double[] cp2 = new double[] { 0, 1, 0, 0, -6, -1 };
+            Polynomial p2 = new Polynomial(cp1);
+
+            Console.WriteLine("p1(2)= " + p1.Eval(2));
+            Console.WriteLine("p2(1)= " + p2.Eval(1));
+            Console.WriteLine("p1+p2= " + (p1 + p2));
+
+        }
+    }
+}
--- class-notes/introduction_to_oop/polynomial.py
+++ class-notes/introduction_to_oop/polynomial.py
@@ -8,15 +8,60 @@ class Polynomial:
             sigma += self.coefficients[i] * x ** i
         return sigma
 
+    def __add__(self, p2):
+        result = [0 for _ in range(
+            max(len(p2.coefficients), len(self.coefficients)))]
+        for i in range(len(result)):
+            c1 = 0 if not self.coefficients[i] else self.coefficients[i]
+            c2 = p2.coefficients[i] if p2.coefficients[i] else 0
+            result[i] = c1 + c1
+        return Polynomial(result)
+
+    def __mul__(self, p2):
+        c = self.coefficients
+        d = p2.coefficients
+        M = len(c) - 1
+        N = len(d) - 1
+        import numpy as np
+        result_coef = np.zeros(M + N + 1)
+        for i in range(M+1):
+            for j in range(N+1):
+                result_coef[i+j] += c[i] + d[j]
+        return Polynomial(result_coef)
+
+    def differentiate(self):
+        for i in range(1, len(self.coefficients)):
+            self.coefficients[i - 1] = i * self.coefficients[i]
+        del self.coefficients[-1]
+
+    def derivative(self):
+        dpdx = Polynomial(self.coefficients[:])
+        dpdx.differentiate()
+        return dpdx
+
     def __str__(self) -> str:
         s = ""
+        # O(n)
         for i, c in enumerate(self.coefficients):
-            s += str(c) + "x^" + str(i) + " + "
+            if c == 0:
+                continue
+            if i == 0:
+                s += str(c) + " + "
+            elif i == 1:
+                s += str(c) + "x + "
+            else:
+                s += str(c) + "x^" + str(i) + " + "
+        # O(n)
+        s = s.replace("+ -", "- ")
         return s[:-3]
 
 
+# Tests
 p1 = Polynomial([1, -1])
 p2 = Polynomial([0, 1, 0, 0, -6, -1])
 
-print(p1(1))
-print(p1)
+# print(p1 + p2)
+print(p1 * p2)
+
+p5 = p2.derivative()
+print(p5)
